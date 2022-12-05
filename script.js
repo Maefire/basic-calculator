749f1d35031bc492405e557faf4c10dcba8dd220
MaeFire <masked_phoenix@yahoo.com>
2022-12-05 06:20:55 -0600

    Add number storage/basic operator input
    while not complete, operator will now put current information into
    a stored display when clicked.

--- script.js
+++ script.js
@@ -8,26 +8,30 @@
     Can we store variables in an object, and read that variable later?
     Change object variables depending on if a class exists on HTML?
 */
-const numButt   = document.querySelectorAll(".number");
-const opButt    = document.querySelectorAll(".operator");
+const numButt = document.querySelectorAll(".number");
+const opButt = document.querySelectorAll(".operator");
 const clearButt = document.querySelector(".allClear");
 const totalButt = document.querySelector(".tally");
-const deButt    = document.querySelector(".decimal");
-const output    = document.querySelector(".output");
-const backButt    = document.querySelector(".backspace");
+const deButt = document.querySelector(".decimal");
+const display = document.querySelector(".currentOutput");
+const lastOutput = document.querySelector(".lastOutput");
+const backButt = document.querySelector(".backspace");
 
 /*DEFAULT Display Output*/
-output.textContent = 0
+display.textContent = "0"
+let storage
+num1 = "";
+num2 = "";
 
 /** Number Calculation Function**/
 function calculate(array, operator) {
-    if(operator === "+"){
-    	return array.reduce((total, current) => total + current);
-    }else if(operator === "-"){
+    if (operator === "+") {
+        return array.reduce((total, current) => total + current);
+    } else if (operator === "-") {
         return array.reduce((total, current) => total - current);
-    }else if(operator === "*"){
+    } else if (operator === "*") {
         return array.reduce((total, current) => total * current);
-    }else if(operator === "/"){
+    } else if (operator === "/") {
         return array.reduce((total, current) => total / current);
     }
 };
@@ -41,38 +45,52 @@ function operate(num1, num2, operator) {
         case "*":
             return calculate([num1, num2], operator);
         case "/":
-            return calculate([num1, num2], operator);
+            if (num2 === 0) {
+                alert("You can't divide by 0.")
+                return
+            } else {
+                return calculate([num1, num2], operator);
+            }
     }
 };
 
 /** Button Operations **/
-numButt.forEach ((number) => {
-    number.onclick = () => {
-        if(output.textContent === "0"){
-           output.textContent = ""; 
+numButt.forEach((button) => {
+    button.onclick = () => {
+        if (display.textContent === "0") {
+            display.textContent = "";
         }
-        output.textContent += number.textContent;
+        display.textContent += button.textContent;
     }
 })
 
-opButt.forEach ((operator) => {
-    operator.onclick = () => {
-        console.log(operator.textContent)
+opButt.forEach((button) => {
+    button.onclick = () => {
+        // console.log(button.textContent)
+        if (lastOutput.textContent !== "") {
+            //store current output as num2 (thiis gives us num1 and num 2)
+            //operate(num1, num2, operator)
+            //console.log operate to see what is returned
+            //lastOutput.textContent = whatever operate returns
+            //store operator (assuming "=" wasn't clicked)
+            //store answer as num1
+            //return
+        } else {
+            lastOutput.textContent = (display.textContent + " " + button.textContent)
+            //store operator
+            //store num1
+            display.textContent = "0"
+        }
     }
 })
 
 backButt.onclick = () => {
-    output.textContent = output.textContent
-                        .substring(0, output.textContent.length - 1)
-    if(output.textContent === ""){
-        output.textContent = 0;
+    display.textContent = display.textContent.substring(0, display.textContent.length - 1)
+    if (display.textContent === "") {
+        display.textContent = 0;
     }
 }
 
-/*Object for storing values? */
-let mathStorage = [
-    {
-        "first operand" : 0,
-        "second operand": 0,
-    }
-]
+clearButt.onclick = () => {
+    window.location.reload();
+}
